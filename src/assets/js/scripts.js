938595b5ef01347b62fd780b8fdead22b3eeedb1
Jasonli <lizhiyong1024@126.com>
2018-04-14 11:45:36 +0800

    NYC 1th verstion

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,10 @@
+// Your custom JavaScript goes here
+
+$(document).ready(function(){
+    $(document).foundation();
+    // var options = {vOffset: 50,hoverDelay:3000};
+    // var elem = new Foundation.Dropdown($(".panelMenu"), options);
+    
+    // var element = $("#mytooltip");
+    // var elem = new Foundation.Tooltip(element, {});
+})
